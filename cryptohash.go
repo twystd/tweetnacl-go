31e3aa3c1bd3bb2bfed273857fcd954540b94907
tonys <tony.seebregts@gmail.com>
2016-04-07 13:25:04 +0200

    crypto_hash: implemented crypto_hashblocks

--- cryptohash.go
+++ cryptohash.go
@@ -32,3 +32,26 @@ func CryptoHash(message []byte) ([]byte, error) {
 
 	return nil, fmt.Errorf("Error calculating SHA-512 hash (error code %v)", rc)
 }
+
+// Wrapper function for crypto_hashblocks.
+//
+// Undocumented anywhere, but seems to be a designed to calculate the SHA-512 hash of
+// a stream of blocks.
+//
+// Ref. http://nacl.cr.yp.to/hash.html
+func CryptoHashBlocks(iv, blocks []byte) ([]byte, error) {
+	hash := make([]byte, crypto_hash_HASH_BYTES)
+	N := len(blocks)
+
+	copy(hash, iv)
+
+	rc := C.crypto_hashblocks((*C.uchar)(unsafe.Pointer(&hash[0])),
+		(*C.uchar)(unsafe.Pointer(&blocks[0])),
+		(C.ulonglong)(N))
+
+	if rc == 0 {
+		return hash, nil
+	}
+
+	return nil, fmt.Errorf("Error calculating running SHA-512 hash (error code %v)", rc)
+}
