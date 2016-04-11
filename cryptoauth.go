9d53f46b80707bb23a967f5e9270f4e67690ff20
tonys <tony.seebregts@gmail.com>
2016-04-11 10:56:02 +0200

    cryptoauth: implemented scalar_mult

--- cryptoauth.go
+++ cryptoauth.go
@@ -15,6 +15,12 @@ const ONETIMEAUTH_BYTES int = 16
 // The number of bytes in the secret key used to generate the authenticator.
 const ONETIMEAUTH_KEYBYTES int = 32
 
+// The number of bytes in the group element component of scalar multiplication.
+const SCALARMULT_BYTES int = 32
+
+// The number of bytes in the integer component of scalar multiplication.
+const SCALARMULT_SCALARBYTES int = 32
+
 // Wrapper function for crypto_onetimeauth.
 //
 // Uses the supplied secret key to calculate an authenticator for the message.
@@ -54,4 +60,20 @@ func CryptoOneTimeAuthVerify(authenticator, message, key []byte) (bool, error) {
 	}
 
 	return false, fmt.Errorf("Error calculating one time authenticator (error code %v)", rc)
+}
+
+// Wrapper function for crypto_scalarmult_base.
+//
+// Computes the scalar product of a standard group element and an integer.
+//
+// Ref. http://nacl.cr.yp.to/onetimeauth.html
+func CryptoScalarMult(n, p []byte) ([]byte, error) {
+	q := make([]byte, SCALARMULT_BYTES)
+	rc := C.crypto_scalarmult(makePtr(q), makePtr(n), makePtr(p))
+
+	if rc == 0 {
+		return q, nil
+	}
+
+	return nil, fmt.Errorf("Error calculating scalar multiplication (error code %v)", rc)
 }
