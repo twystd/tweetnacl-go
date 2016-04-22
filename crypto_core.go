a3b77926273b83529f313444442a0f0984fddb8a
tonys <tony.seebregts@gmail.com>
2016-04-22 12:46:52 +0200

    crypto_core: added argument validation to CryptoCoreHSalsa20

--- crypto_core.go
+++ crypto_core.go
@@ -10,10 +10,28 @@ import (
 )
 
 // The number of bytes in an HSALSA20 intermediate key.
-const crypto_core_HSALSA20_OUTPUTBYTES int = 32
+const HSALSA20_OUTPUTBYTES int = 32
 
-// The number of bytes in an SALSA20 intermediate key.
-const crypto_core_SALSA20_OUTPUTBYTES int = 64
+// The number of bytes in the shared secret for crypto_core_hsalsa20.
+const HSALSA20_INPUTBYTES int = 16
+
+// The number of bytes in the secret keyfor crypto_core_hsalsa20.
+const HSALSA20_KEYBYTES int = 32
+
+// The number of bytes in the constant for crypto_core_hsalsa20.
+const HSALSA20_CONSTBYTES int = 16
+
+// The number of bytes in the calculated intermediate key.
+const SALSA20_OUTPUTBYTES int = 64
+
+// The number of bytes in the shared secret for crypto_core_salsa20.
+const SALSA20_INPUTBYTES int = 16
+
+// The number of bytes in the secret key for crypto_core_salsa20.
+const SALSA20_KEYBYTES int = 32
+
+// The number of bytes in the constant for crypto_core_salsa20.
+const SALSA20_CONSTBYTES int = 16
 
 // Wrapper function for crypto_core_hsalsa20.
 //
@@ -21,8 +39,25 @@ const crypto_core_SALSA20_OUTPUTBYTES int = 64
 // intermediate key (from a secret key and shared secret) for encrypting and
 // authenticating packets.
 //
+// in is a HSALSA20_INPUTBYTES byte array containing the shared secret.
+// key is a HSALSA20_KEYBYTES byte array containing the secret key.
+// constant is a HSALSA20_CONSTBYTES byte array containing an apparently arbitrary 'constant'
+// (IV ?) to be used for the intermediate key calculation.
+//
 func CryptoCoreHSalsa20(in, key, constant []byte) ([]byte, error) {
-	out := make([]byte, crypto_core_HSALSA20_OUTPUTBYTES)
+	if len(in) != HSALSA20_INPUTBYTES {
+		return nil, fmt.Errorf("Error calculating HSALSA20 intermediate key (%v)", "invalid shared secret")
+	}
+
+	if len(key) != HSALSA20_KEYBYTES {
+		return nil, fmt.Errorf("Error calculating HSALSA20 intermediate key (%v)", "invalid secret key")
+	}
+
+	if len(constant) != HSALSA20_CONSTBYTES {
+		return nil, fmt.Errorf("Error calculating HSALSA20 intermediate key (%v)", "invalid constant")
+	}
+
+	out := make([]byte, HSALSA20_OUTPUTBYTES)
 
 	rc := C.crypto_core_hsalsa20(makePtr(out),
 		makePtr(in),
@@ -43,7 +78,7 @@ func CryptoCoreHSalsa20(in, key, constant []byte) ([]byte, error) {
 // authenticating packets.
 //
 func CryptoCoreSalsa20(in, key, constant []byte) ([]byte, error) {
-	out := make([]byte, crypto_core_SALSA20_OUTPUTBYTES)
+	out := make([]byte, SALSA20_OUTPUTBYTES)
 
 	rc := C.crypto_core_salsa20(makePtr(out),
 		makePtr(in),
